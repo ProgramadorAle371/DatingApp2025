6390ff0407ff3c4fea0ec0bcfcc4d9c2bcd6aa30
ProgramadorAle371 <programadoralejandro371@gmail.com>
2025-09-10 01:02:15 -0600

    Parcial01 -  Configuration Controllers

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers;
 
 [ApiController]
-[Route("api/v1/[controller]")]
+[Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
