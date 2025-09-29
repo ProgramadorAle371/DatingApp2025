a566f9df00f9bd96065779cde686ca9e9a26cf9f
ProgramadorAle371 <programadoralejandro371@gmail.com>
2025-09-29 10:20:56 -0600

    Parcial02 - Add Pesistence to Login User

---
+++ client/src/types/user.ts
@@ -0,0 +1,26 @@
+export type User = {
+  username: string;
+  displayName: string;
+  email: string;
+  token: string;
+  image?: string;
+}
+
+export interface IUser {
+  username: string;
+  displayName: string;
+  email: string;
+  token: string;
+  image?: string;
+}
+
+export type LoginCreds = {
+  email: string;
+  password: string;
+}
+
+export type RegisterCreds = {
+  email: string;
+  displayName: string;
+  password: string;
+}
