6abe6a69e1a71f5ccccc1bc856a2fdb27cc0e26f
ProgramadorAle371 <programadoralejandro371@gmail.com>
2025-10-21 07:31:02 -0600

    Parcial02 - Add JsonIgnore  ti simulate mapper

--- API/Data/MembersRepository.cs
+++ API/Data/MembersRepository.cs
@@ -15,7 +15,9 @@ public class MembersRepository(AppDbContext context) : IMembersRepository
 
     public async Task<IReadOnlyList<Member>> GetMembersAsync()
     {
-        return await context.Members.ToListAsync();
+        return await context.Members
+        //.Include(m => m.Photos)
+        .ToListAsync();
     }
 
     public async Task<IReadOnlyList<Photo>> GetPhotoAsync(string memberId)
