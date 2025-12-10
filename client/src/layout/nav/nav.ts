c253f16fcbede890a6d8740a7681b393e894513d
ProgramadorAle371 <programadoralejandro371@gmail.com>
2025-12-10 13:48:31 -0600

    Parcial03 - Add theme manager

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,9 +1,10 @@
 import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
-import { Router, RouterLink } from "@angular/router";
-import { RouterLinkActive } from "@angular/router";
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ToastService } from '../../core/services/toast-service';
+import { themes } from '../theme';
 
 @Component({
   selector: 'app-nav',
@@ -12,17 +13,34 @@ import { ToastService } from '../../core/services/toast-service';
   styleUrl: './nav.css'
 })
 export class Nav {
-  protected accountService = inject(AccountService);
-  protected creds: any = {};
+export class Nav implements OnInit {
   private router = inject(Router);
   private toast = inject(ToastService);
+  protected accountService = inject(AccountService);
+  protected creds: any = {};
+  protected selectedTheme = signal<string>(localStorage.getItem("theme") || "light");
+  protected themes = themes;
+
+  ngOnInit(): void {
+    document.documentElement.setAttribute("data-theme", this.selectedTheme());
+  }
+
+  handleSelectedTheme(theme: string) {
+    this.selectedTheme.set(theme);
+    localStorage.setItem("theme", theme);
+    document.documentElement.setAttribute("data-theme", theme);
+    const elem = document.activeElement as HTMLDivElement;
+    if (elem) {
+      elem.blur();
+    }
+  }
 
   login(): void {
     this.accountService.login(this.creds).subscribe({
       next: response => {
-        this.router.navigateByUrl('/members');
+        this.router.navigateByUrl("/members");
         this.creds = {};
-        this.toast.success("Login successful");
+        this.toast.success("Logged in!")
       },
       error: error => {
         this.toast.error(error.error);
@@ -32,6 +50,6 @@ export class Nav {
 
   logout(): void {
     this.accountService.logout();
-    this.router.navigateByUrl('/');
+    this.router.navigateByUrl("/");
   }
 }
