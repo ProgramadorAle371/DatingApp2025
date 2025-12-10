99470ee1067d79cf41956d7432948bdfc97b7a6a
ProgramadorAle371 <programadoralejandro371@gmail.com>
2025-12-10 13:19:08 -0600

    Parcial03 - Add member detail child components

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -8,16 +8,26 @@ import { authGuard } from '../core/guard/auth-guard';
 import { TestErrors } from '../features/test-errors/test-errors';
 import { NotFound } from '../shared/errors/not-found/not-found';
 import { ServerError } from '../shared/errors/server-error/server-error';
+import { MemberProfile } from '../features/member-profile/member-profile';
+import { MemberPhotos } from '../features/member-photos/member-photos';
+import { MemberMessages } from '../features/member-messages/member-messages';
 
 export const routes: Routes = [
     { path: "", component: Home},
     {
-        path: "",
-        runGuardsAndResolvers: "always",
         canActivate: [authGuard],
         children: [
-            { path:"members", component: MemberList, canActivate: [authGuard]},
-            { path: "memmbers/:id", component: MemberDetail},
+            { path:"members", component: MemberList},
+            { 
+                path: "memmbers/:id", 
+                component: MemberDetail,
+                    children: [
+                        { path: "", redirectTo: "profile", pathMatch: "full" },
+                        { path: "profile", component: MemberProfile, title: "Profile"},
+                        { path: "photos", component: MemberPhotos, title: "Photos"},
+                        { path: "messages", component: MemberMessages, title: "Messages"},
+                    ]
+            },
             { path: "lists", component: Lists},
             { path: "messages", component: Messages},
         ]
--- client/src/features/members/member-detail/member-detail.ts
+++ client/src/features/members/member-detail/member-detail.ts
@@ -1,33 +1,42 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MembersService } from '../../../core/services/members-service';
 import { Member } from '../../../types/member';
-import { ActivatedRoute, Router, RouterLink, RouterLinkActive } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRoute, Router, RouterLink, RouterLinkActive, NavigationEnd, RouterOutlet } from '@angular/router';
+import { Observable, filter } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-member-detail',
-  imports: [AsyncPipe, RouterLink, RouterLinkActive],
+  imports: [AsyncPipe, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './member-detail.html',
   styleUrl: './member-detail.css'
 })
 export class MemberDetail implements OnInit {
   private membersService = inject(MembersService);
   private route = inject(ActivatedRoute);
+  private router = inject(Router);
   protected member$!: Observable<Member>;
 
   ngOnInit(): void {
-    this.member$ = this.loadMember();
-  }
+      this.member$ = this.loadMember();
+      this.title.set(this.route.firstChild?.snapshot?.title);
 
-  loadMember(): Observable<Member> | undefined {
-    const id = this.route.snapshot.paramMap.get("id");
-
-    if (id) {
-      return this.membersService.getMember(id);
+      this.router.events.pipe(
+        filter(event => event instanceof NavigationEnd)
+      ).subscribe({
+        next: () => {
+          this.title.set(this.route.firstChild?.snapshot?.title);
+        }
+      });
     }
 
-    return;
-  }
+    loadMember(): Observable<Member> | undefined {
+      const id = this.route.snapshot.paramMap.get("id");
 
+      if (id) {
+        return this.membersService.getMember(id);
+      }
+
+      return;
+    }
 }
